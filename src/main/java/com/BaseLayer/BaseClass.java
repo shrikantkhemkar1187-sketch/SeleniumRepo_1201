758cbb13dc27197b933c5d45a4eee3187828e875
shrikantkhemkar1187-sketch <shirkantkhemkar1187@gmail.com>
2026-01-12 17:09:21 +0530

    Added Selenium Dependency, Created BaseLayer package in src main and BaseClass in BaseLayer

---
+++ src/main/java/com/BaseLayer/BaseClass.java
@@ -0,0 +1,13 @@
+package com.BaseLayer;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+	
+	public static void initialization()
+	{
+		
+	}
+}
